7ee389d384b8d0d275c5b2d4f3b009f4947d801e
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-09-08 00:15:04 +0000

    added sentences to story.c

--- story.c
+++ story.c
@@ -28,6 +28,8 @@ int main( void )
 	skh6yfSentence2();
 	ampwd6Sentence1();
 	ampwd6Sentence2();
+	jrf5x8Sentence1();
+	jrf5x8Sentence2();
 	// DO NOT ADD AFTER THIS COMMENT
 	// Bens sentence should be last.
 	bensSentence_FINAL();
