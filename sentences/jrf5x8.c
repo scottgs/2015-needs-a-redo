76a342444e9aced817513d387512d936a9452f1f
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-09-08 00:28:16 +0000

    fixed syntax error in my sentences file

--- sentences/jrf5x8.c
+++ sentences/jrf5x8.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int jrf5x8Sentence1(){
-        printf("Bigfoot couldn't stop talking about how he likes to push and ba$
+        printf("Bigfoot couldn't stop talking about how he likes to push and bash.\n");
         return 1;
 }
 
 int jrf5x8Sentence2(){
-        printf("But then comes along the Devil Bird and says lets git down to b$
+        printf("But then comes along the Devil Bird and says lets git down to business\n");
         return 1;
 }
 
