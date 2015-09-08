1494b086d231cbc0d19f8e1d7eefd62315ce505e
Tyler Brown <tmb4qf@mail.missouri.edu>
2015-09-08 11:03:24 -0500

    Add tmb4qf sentences

--- sentences/tmb4qf.c
+++ sentences/tmb4qf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int jrf5x8Sentence1(){
+int tmb4qfSentence1(){
         printf("Then Bigfoot and the Devil Bird start singing \"Let it go\" from Frozen.\n");
         return 1;
 }
 
-int jrf5x8Sentence2(){
+int tmb4qfSentence2(){
         printf("But Linus brashly tells them to shut up (in typical Linus fashion).\n");
         return 1;
 }
