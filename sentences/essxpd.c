96f671983d5f2b2ed9580c899b23b5ba4a69bb10
essxpd <essxpd@mail.missouri.edu>
2015-09-09 10:33:42 -0500

    Added my sentences.

---
+++ sentences/essxpd.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int essxpdSentence1()
+{
+  printf("Sensi SSH unfortunately responded \"ssh: Could not resolve toast.\"\n");
+  return 1;
+}
+
+int essxpdSentence2()
+{
+  printf("The ninja realized the error of his ways and new that he needed his keys to toast those cannolis.\n");
+  return 1;
+}
+
+
+
--- story.c
+++ story.c
@@ -44,6 +44,8 @@ int main( void )
 	mdvy96Sentence2();
 	dmlcn6Sentence1();
 	dmlcn6Sentence2();
+	essxpdSentence1();
+	essxpdSentence2();
 	// DO NOT ADD AFTER THIS COMMENT
 	// Bens sentence should be last.
 	bensSentence_FINAL();
