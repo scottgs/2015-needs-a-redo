237a447eadb3a84466c66293fae6cd9d96e1653e
mth522 <mth522@mail.missouri.edu>
2015-09-10 10:53:35 -0500

    sentences commit

---
+++ sentences/mth522.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int mth522Sentence1(){
+printf ("The Ogopogo was a great master protector.\n");
+return 1;
+}
+int mth522Sentence2(){
+printf ("Everyone loved him-including the Jackalopes.\n");
+return 1;
+}
+
