640e277a0e05c495080c7c8d09272512be35144b
Piper Williams <pswnr9@mail.missouri.edu>
2015-09-10 17:47:55 -0500

    Create pswnr9.c

---
+++ sentences/pswnr9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int pswnr9Sentence1(){
+	printf("Then the Bear Lake Monster came with 10 hambergers from steak and shake.\n");
+	return 1;
+
+}
+
+int pswnr9Sentence2(){
+	printf("He decided to share half of them with big foot because they're best friends.\n");
+	return 1;
+
+}
