a5094815423cf6b75c404bb108c5f181d227c0ca
anmg8 <anmg8@mail.missouri.edu>
2015-09-11 19:31:41 -0700

    added file

---
+++ sentences/anmg8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+
+int anmg8Sentence1()
+	{
+	printf("The Devil Bird began to git tired of all the git jokes.\n");
+	return 1;
+	}
+
+int anmg8Sentence2()
+	{
+	printf("But not even the Deer Lord could save them all\n");
+	return 1;
+	}
