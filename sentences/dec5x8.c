9ca47e1f378a4cae7fbaecabfb6635bf998216e3
Austin Guffey <afguffey99@localhost.localdomain>
2015-09-11 19:56:07 -0500

    added sentences/dec5x7.c (didn't exist)

---
+++ sentences/dec5x8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int dec5x8Sentence1() {
+	printf("dec5x8 didn't have a sentence file...\n");
+	return 1;
+}
+
+int dec5x8Sentence2() {
+	printf("...So I made this one.\n");
+	return 1;
+}
